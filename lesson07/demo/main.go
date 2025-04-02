4f9cbbd436858c2e410192ed6daa6cecc8647e35
yangkai.04 <yangkai.04@bytedance.com>
2025-04-02 22:47:32 +0800

    feat: pb write/read delim

--- lesson07/demo/main.go
+++ lesson07/demo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 
@@ -9,11 +10,19 @@ import (
 	"demo/proto/sniff"
 
 	"google.golang.org/protobuf/encoding/protodelim"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
-	println(decodeFromFile("./part-6-0"))
-	println(decodeFromFile("./part-3-0"))
+	if decodeFromFile("./part-6-0") != 1 {
+		panic("decodeFromFile() != 1")
+	}
+	if decodeFromFile("./part-3-0") != 2 {
+		panic("decodeFromFile() != 2")
+	}
+	if writeBatchThenDecode() != 10 {
+		panic("writeBatchThenDecode() != 10")
+	}
 }
 
 func foo() {
@@ -22,6 +31,14 @@ func foo() {
 		SalePrice:   80,
 	}
 	fmt.Println(price)
+}
+
+func printJson(sniffList []*sniff.ReqResp) {
+	for _, reqResp := range sniffList {
+		v, _ := protojson.Marshal(reqResp)
+		fmt.Printf("%s\n", v)
+	}
+
 }
 
 func decodeFromFile(fileName string) int {
@@ -41,6 +58,32 @@ func decodeFromFile(fileName string) int {
 		}
 		sniffList = append(sniffList, reqResp)
 	}
-	fmt.Println(sniffList)
+	printJson(sniffList)
+	println()
+	return len(sniffList)
+}
+
+func writeBatchThenDecode() int {
+	buf := &bytes.Buffer{}
+	for i := 0; i < 10; i++ {
+		reqResp := &sniff.ReqResp{}
+		reqResp.Id = fmt.Sprintf("taskId-%d", i)
+		reqResp.Request = []byte("abc")
+		if n, err := protodelim.MarshalTo(buf, reqResp); err != nil {
+			panic(fmt.Sprintf("protodelim.MarshalTo(_, %v) = %d, %v", reqResp, n, err))
+		}
+	}
+
+	var sniffList []*sniff.ReqResp
+	for {
+		reqResp := &sniff.ReqResp{}
+		err := protodelim.UnmarshalFrom(buf, reqResp)
+		if err != nil {
+			break
+		}
+		sniffList = append(sniffList, reqResp)
+	}
+	printJson(sniffList)
+	println()
 	return len(sniffList)
 }
